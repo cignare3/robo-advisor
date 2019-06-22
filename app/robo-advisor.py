7a7ac0a5a18b5142181f5d15a90cfa3d20f8640d
cignare3 <50918984+cignare3@users.noreply.github.com>
2019-06-22 17:19:46 -0400

    writing example to csv

--- app/robo-advisor.py
+++ app/robo-advisor.py
@@ -47,7 +47,15 @@ recent_low = min(low_prices)
 #stock_symbol = input("Please enter stock symbol: ")
 #if stock_symbol.isalpha():  
    
+csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
 
+with open(csv_file_path, "w") as csv_file:
+    writer = csv.DictWriter(csv_file, fieldnames=["city", "name"])
+    writer.writeheader() # uses fieldnames set above
+    writer.writerow({"city": "New York", "name": "Yankees"})
+    writer.writerow({"city": "New York", "name": "Mets"})
+    writer.writerow({"city": "Boston", "name": "Red Sox"})
+    writer.writerow({"city": "New Haven", "name": "Ravens"})
 
     
 #else:
@@ -67,5 +75,9 @@ print("-------------------------")
 print("RECOMMENDATION: BUY!")
 print("RECOMMENDATION REASON: TODO")
 print("-------------------------")
+print(f"WRITING DATA TO CSV... {csv_file_path}")
+print("-------------------------")
 print("HAPPY INVESTING!")
 print("-------------------------")
+
+
