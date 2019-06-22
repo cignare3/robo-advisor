2ec169074e40bbc447c48c9288b315699975207e
cignare3 <50918984+cignare3@users.noreply.github.com>
2019-06-22 15:10:22 -0400

    last refreshed

--- app/robo-advisor.py
+++ app/robo-advisor.py
@@ -12,16 +12,18 @@ import requests
 #request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={stock_symbol}&apikey={API_KEY}"
 request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=P40N2DKCG8YHQQB4"
 response = requests.get(request_url)
-print(type(response))
-print(response.status_code)
-print(response.text)
+#print(type(response))
+#print(response.status_code)
+#print(response.text)
 
 parsed_response = json.loads(response.text)
-breakpoint()
 
-quit()
 
-stock_symbol = input("Please enter stock symbol: ")
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+
+#breakpoint()
+
+#stock_symbol = input("Please enter stock symbol: ")
 #if stock_symbol.isalpha():  
    
 
@@ -34,7 +36,7 @@ print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm")
+print(f"REQUEST AT: {last_refreshed}")
 print("-------------------------")
 print("LATEST DAY: 2018-02-20")
 print("LATEST CLOSE: $100,000.00")
